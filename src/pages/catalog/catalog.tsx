b1b718c9580fca56f688e2929ccd13efd4747873
lexusegorov <alexey.egorov.job@gmail.com>
2022-10-04 14:51:54 +0700

    feat: add catalog (static)

---
+++ src/pages/catalog/catalog.tsx
@@ -0,0 +1,32 @@
+import Banner from '../../components/banner/banner';
+import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
+import CatalogFilter from '../../components/catalog-filter/catalog-filter';
+import CatalogSort from '../../components/catalog-sort/catalog-sort';
+import Pagination from '../../components/pagination/pagination';
+import ProductCard from '../../components/product-card/product-card';
+
+function Catalog() : JSX.Element {
+  return (
+    <>
+      <Banner />
+      <Breadcrumbs />
+      <section className="catalog">
+        <div className="container">
+          <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
+          <div className="page-content__columns">
+            <CatalogFilter />
+            <div className="catalog__content">
+              <CatalogSort />
+              <div className="cards catalog__cards">
+                <ProductCard />
+              </div>
+              <Pagination />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Catalog;
