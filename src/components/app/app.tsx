5d6c4527ed4ccb25cd19c3a6507ea9c79d10b497
lexusegorov <alexey.egorov.job@gmail.com>
2022-10-04 12:38:05 +0700

    feat: add header, footer, layout

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,16 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import Layout from '../../pages/layout/layout';
+
 function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={AppRoute.Root} element={<Layout />}>
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
