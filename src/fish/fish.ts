25f16229e0000772c1e57a826f0df0e5c8e54859
lexusegorov <alexey.egorov.job@gmail.com>
2022-10-05 12:12:32 +0700

    feat: add types, mocks

---
+++ src/fish/fish.ts
@@ -0,0 +1,45 @@
+import { Cameras, Promo } from '../types/types';
+
+export const promo : Promo = {
+  id: 1,
+  name: 'Ретрокамера Dus Auge lV',
+  previewImg: 'img/content/promo.jpg',
+  previewImg2x: 'img/content/promo@2x.jpg',
+  previewImgWebp: 'img/content/promo.webp',
+  previewImgWebp2x: 'img/content/promo@2x.webp'
+};
+
+export const products : Cameras = [
+  {
+    id: 1,
+    name: 'Ретрокамера Dus Auge lV',
+    vendorCode: 'DA4IU67AD5',
+    type: 'Коллекционная',
+    category: 'Видеокамера',
+    description: 'Немецкий концерн BRW разработал видеокамеру Das Auge IV в начале 80-х годов, однако она до сих пор пользуется популярностью среди коллекционеров и яростных почитателей старинной техники.',
+    level: 'Любительский',
+    rating: 4,
+    price: 73450,
+    previewImg: 'img/content/das-auge.jpg',
+    previewImg2x: 'img/content/das-auge@2x.jpg',
+    previewImgWebp: 'img/content/das-auge.webp',
+    previewImgWebp2x: 'img/content/das-auge@2x.webp',
+    reviewCount: 16,
+  },
+  {
+    id: 2,
+    name: 'Ретрокамера',
+    vendorCode: 'DA4IU67AD5',
+    type: 'Коллекционная',
+    category: 'Видеокамера',
+    description: 'Немецкий концерн BRW разработал видеокамеру Das Auge IV в начале 80-х годов, однако она до сих пор пользуется популярностью среди коллекционеров и яростных почитателей старинной техники.',
+    level: 'Любительский',
+    rating: 4,
+    price: 450,
+    previewImg: 'img/content/das-auge.jpg',
+    previewImg2x: 'img/content/das-auge@2x.jpg',
+    previewImgWebp: 'img/content/das-auge.webp',
+    previewImgWebp2x: 'img/content/das-auge@2x.webp',
+    reviewCount: 10,
+  },
+];
