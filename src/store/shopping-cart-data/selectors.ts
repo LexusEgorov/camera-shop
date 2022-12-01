34632d8a94017e0f42322b73a5463f2124e14e2a
lexusegorov <alexey.egorov.job@gmail.com>
2022-12-01 18:43:39 +0700

    fix: store selector

--- src/store/shopping-cart-data/selectors.ts
+++ src/store/shopping-cart-data/selectors.ts
@@ -5,10 +5,6 @@ const NOT_FOUND = -1;
 
 export const getCartProducts = (state: State) : ShoppingCartItem[] => state[NameSpace.ShoppingCartData].products;
 
-export const getCartCountItems = (state: State) : number => {
-  const products = state[NameSpace.ShoppingCartData].products;
-
-  return 0;
-};
+export const getCartCountItems = (state: State) : number => 0;
 
 export const getCartHasProduct = (id: number) => (state: State) : boolean => state[NameSpace.ShoppingCartData].products.findIndex((product) => product.id === id) !== NOT_FOUND;
